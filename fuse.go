4175926eb9b27d8dab43963fa783e991449f58d2
gurvin01 <vinay198x@gmail.com>
2023-05-02 09:14:41 -0600

    Updated code and removed mod file

--- fuse.go
+++ fuse.go
@@ -9,53 +9,37 @@ import (
 )
 
 // generateFuseFromProject - Generates a fuse file by reading a kitewheel project
-func (c *Client) GenerateFuseFromProject(ctx context.Context, xpProject XPProject) (status bool, err error) {
+func (c *Client) GenerateFuseFromProject(ctx context.Context, xpProject XPProject) {
 	rb, err := json.Marshal(xpProject)
 	if err != nil {
 		panic(err)
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/fuse", c.HostURL), strings.NewReader(string(rb)))
-	if err != nil {
-		return false, err
-	}
-
-	body, err := c.doRequest(req)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/fusefromproject", c.HostURL), strings.NewReader(string(rb)))
 	if err != nil {
 		panic(err)
 	}
 
-	responseStatus := status
-	err = json.Unmarshal(body, &responseStatus)
+	c.doRequest(req)
 	if err != nil {
-		return false, err
+		panic(err)
 	}
-
-	return responseStatus, nil
 }
 
 // generateFuseFromProject - Generates a fuse file by reading a kitewheel project
-func (c *Client) GenerateProjectFromFuse(ctx context.Context, xpProject XPProject) (status bool, err error) {
+func (c *Client) GenerateProjectFromFuse(ctx context.Context, xpProject XPProject) {
 	rb, err := json.Marshal(xpProject)
 	if err != nil {
 		panic(err)
 	}
 
-	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/fuse", c.HostURL), strings.NewReader(string(rb)))
-	if err != nil {
-		return false, err
-	}
-
-	body, err := c.doRequest(req)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/projectfromfuse", c.HostURL), strings.NewReader(string(rb)))
 	if err != nil {
 		panic(err)
 	}
 
-	responseStatus := status
-	err = json.Unmarshal(body, &responseStatus)
+	c.doRequest(req)
 	if err != nil {
-		return false, err
+		panic(err)
 	}
-
-	return responseStatus, nil
 }
