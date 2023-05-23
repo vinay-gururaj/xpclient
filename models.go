b6910ae58b2fc2d133e49f0865871c0f4148983b
gurvin01 <vinay198x@gmail.com>
2023-05-23 16:17:16 -0600

    Experiment - Updated mod file

--- models.go
+++ models.go
@@ -2,8 +2,6 @@ package xpclient
 
 import (
 	"time"
-
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type XPProject struct {
@@ -684,13 +682,13 @@ type KWPrototype struct {
 }
 
 type JavaScript struct {
-	Name         string     `json:"name,omitempty"`
-	Id           string     `json:"id,omitempty"`
-	ProjectId    string     `json:"projectId,omitempty"`
-	Code         string     `json:"code,omitempty"`
-	ArgumentList types.List `json:"argumentList,omitempty"`
+	Name         string   `json:"name,omitempty"`
+	Id           string   `json:"id,omitempty"`
+	ProjectId    string   `json:"projectId,omitempty"`
+	Code         string   `json:"code,omitempty"`
+	ArgumentList []string `json:"argumentList,omitempty"`
 	// ArgumentList string `json:"argumentList,omitempty"`
-	Tags types.List `json:"tags,omitempty"`
+	Tags []string `json:"tags,omitempty"`
 	// Tags           string `json:"tags,omitempty"`
 	SourceScriptId string `json:"sourceScriptId,omitempty"`
 }
